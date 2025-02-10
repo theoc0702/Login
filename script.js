5509478f4a41ce93c234c2c6f501dfe297f6f68c
Theo <161455230+theoc0702@users.noreply.github.com>
2025-02-10 19:01:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
- $(document).ready(function() {
+$(document).ready(function() {
     $(".errorall").hide();
     $(".Mailerror").hide();
     $(".Passworderror").hide();
+    
     $('#login-btn').click(function(e) {
         e.preventDefault();
-
+        let hasError = false;
         const email = $("#email").val();
         const password = $("#password").val();
-
+        
         if (email.length === 0) {
             $("#email").next(".Mailerror").show();
+            hasError = true;
         } else {
             $("#email").next(".Mailerror").hide();
         }
-
+        
         if (password.length < 8) {
             $("#password").next(".Passworderror").show();
-            } else {
-                 $("#password").next(".Passworderror").hide();
-            }
+            hasError = true;
+        } else {
+            $("#password").next(".Passworderror").hide();
+        }
         
-            if (email.length === 0 && password.length < 8 ) {
-                $(".Mailerror").hide();
-                $(".Passworderror").hide();
-                $(".errorall").show();
-            }
-
-         else{
-                window.location.href = "https://www.youtube.com/watch?v=xvFZjo5PgG0&ab_channel=Duran";
-            }
-
+        if (email.length === 0 && password.length < 8) {
+            $(".Mailerror").hide();
+            $(".Passworderror").hide();
+            $(".errorall").show();
+            hasError = true;
+        }
+        
+        if (!hasError) {
+            window.location.href = "https://www.youtube.com/watch?v=xvFZjo5PgG0&ab_channel=Duran";
+        }
+        
         console.log(`Email length: ${email.length}`);
         console.log(`Password length: ${password.length}`);
     });
